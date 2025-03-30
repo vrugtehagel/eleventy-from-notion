113c1c348aba19981b25ddaebd3ca008662e1cd9
vrugtehagel <vrugtehagel@pm.me>
2025-03-30 10:22:54 +0200

    Expose (export) all user-facing types
    Previously, only the the `EleventyFromNotionOptions` type was exported.
    While this makes sense for projects that have a single descriptive type
    for their options, this package defines it as a union of other types,
    that are not exported. This makes the documentation terrible, because
    none of the _actually_ helpful types are explained or even shown.

--- mod.ts
+++ mod.ts
@@ -1,3 +1,15 @@
 export { Builder as NotionPageBuilder } from "./src/builder.ts";
 export { EleventyFromNotion as default } from "./src/eleventy.ts";
-export type { EleventyFromNotionOptions } from "./src/types/eleventy.ts";
+
+export type {
+  EleventyFromNotionOptions,
+  EleventyOptions,
+} from "./src/types/eleventy.ts";
+export type {
+  BlockFormatters,
+  BlockInfo,
+  BuilderOptions,
+  BuildOptions,
+  InlineFormatters,
+} from "./src/types/options.ts";
+export type { FrontMatter } from "./src/types/output.ts";
